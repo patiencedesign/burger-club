92f75a4436f63bb15a8d920e973019fd05369c69
Patience Elliott <elliottpatience7@gmail.com>
2017-06-30 15:35:27 +0000

    Fixed Carousel, Edited Media Page

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 $(document).ready(function() {
-
-    $('#burgerCarousel').carousel()
-    
-
-    $('#signupButton').click(function(){
-        $('#userName').val()
-        
-        $('#userEmail').val()
-        
-        $('.modal-body').append()
-    })
-    
-    $('#signupButton').click(function(){
-        alert('Hey, ' + $('#userName').val() + ' thank you for contacting us! We will get back to ' +  $('#userEmail').val() + ' shortly!');
-    })
-    
-
-$('#email').click(function(){
-    $('#form').val()
-})
-
-$('#email').click(function(){
-    window.location.href = 'http://formspree.io/YOUREMAILHERE'
-})
-
-$("button").click(function(){
-    $.getJSON("https://api.twitter.com/1.1/statuses/user_timeline.json", function(result){
-        $.each(result, function(i, field) {
-            $("div").append(field)
-        })
-    })
-})
-
+	$('#burgerCarousel').carousel()
+	$('#signupButton').click(function() {
+		$('#userName').val()
+		$('#userEmail').val()
+		$('.modal-body').append()
+	})
+	$('#signupButton').click(function() {
+		alert('Hey, ' + $('#userName').val() + ' thank you for contacting us! We will get back to ' + $('#userEmail').val() + ' shortly!');
+	})
+	$('#email').click(function() {
+		$('#form').val()
+	})
+	$('#email').click(function() {
+		window.location.href = 'http://formspree.io/YOUREMAILHERE'
+	})
+	$("button").click(function() {
+		$.getJSON("https://api.twitter.com/1.1/statuses/user_timeline.json", function(result) {
+			$.each(result, function(i, field) {
+				$("div").append(field)
+			})
+		})
+	})
 });
-
-
-    
